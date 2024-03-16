3d199b4a6a5a9b9a64d7518f8b5954cda75f6f43
aninditasarkar678@gmail.com <aninditasarkar678@gmail.com>
2024-03-17 01:46:18 +0530

    Contact form updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,6 +31,25 @@ export const Projects = () => {
       webUrl:"#",
     },
   ];
+  const projects2 = [
+
+    {
+      title: "Social Media App",
+      description: "MERN Stack Development",
+      imgUrl: projImg2,
+      webUrl:"https://www.thoghts.app",
+    },
+   
+  ];
+
+  const projects3 = [
+  {
+      title: "Tour and Travel Website",
+      description: "Full Stack Development",
+      imgUrl: projImg3,
+      webUrl:"#",
+    },
+  ];
 
   return (
     <section className="project" id="projects">
@@ -70,11 +89,33 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
